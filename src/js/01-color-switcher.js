23fbe6d83dc52e64108e740a41c25a4d99a0f43e
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-01-05 11:08:47 +0100

    u

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+let startBtn = document.querySelector('button[data-start]');
+let stopBtn = document.querySelector('button[data-stop]');
+let background = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timer = null;
+stopBtn.disabled = true;
+
+let start = () => {
+  let colorsBg = () => {
+    background.style.backgroundColor = `${getRandomHexColor()}`;
+  };
+  timer = setInterval(colorsBg, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+};
+
+let stop = () => {
+  clearInterval(timer);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+};
+
+startBtn.addEventListener('click', start);
+stopBtn.addEventListener('click', stop);
